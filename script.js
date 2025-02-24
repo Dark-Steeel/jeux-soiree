2a2ea20ac795160e788a9a36511f478b5c166c24
Dark-Steeel <kelian138@gmail.com>
2025-02-24 12:36:33 +0100

    Ajout du mode multijoueur à chaque jeu et suppression du menu multijoueur

--- script.js
+++ script.js
@@ -44,3 +44,40 @@ function nextPlayer() {
         alert("Ajoutez des joueurs pour commencer.");
     }
 }
+let players = [];
+let scores = {};
+let currentPlayerIndex = 0;
+
+
+
+function updatePlayersList() {
+    document.getElementById("playersList").innerText = "Joueurs: " + players.join(", ");
+}
+
+function addPoint() {
+    if (players.length > 0) {
+        let currentPlayer = players[currentPlayerIndex];
+        scores[currentPlayer] += 1;
+        updateScoreBoard();
+    }
+}
+
+function updateScoreBoard() {
+    let scoreboard = document.getElementById("scoreBoard");
+    scoreboard.innerHTML = "";
+    let sortedPlayers = Object.keys(scores).sort((a, b) => scores[b] - scores[a]);
+    sortedPlayers.forEach(player => {
+        let li = document.createElement("li");
+        li.innerText = `${player}: ${scores[player]} points`;
+        scoreboard.appendChild(li);
+    });
+}
+
+function nextPlayer() {
+    if (players.length > 0) {
+        currentPlayerIndex = (currentPlayerIndex + 1) % players.length;
+        alert("C'est au tour de " + players[currentPlayerIndex] + " de jouer !");
+    } else {
+        alert("Ajoutez des joueurs pour commencer.");
+    }
+}
